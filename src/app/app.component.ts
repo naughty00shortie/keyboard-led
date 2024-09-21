7ac25c53caa9163914f4c51d0fc0eded90d3021e
Albertus Van der Walt <albertus@psybergate.co.za>
2024-09-21 18:36:27 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.syncButtonStates();
+    console.log(window.location.hostname);
   }
 
   changeButtonColor(event: Event): void {
